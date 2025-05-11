b6f20b9674e4d33d5a431cbf94281c909ea4684b
NimaiDey <Nimaidey67@gmail.com>
2025-05-11 18:13:42 +0200

    CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/abdul/ros_as_ws/src
-# Build directory: /home/abdul/ros_as_ws/build
+# Source directory: /home/Nimai/assignment/src
+# Build directory: /home/Nimai/assignment/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.

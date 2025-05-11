e9d92f26c76cf253ba1e2c56e9fe2cf1f6ee3218
NimaiDey <Nimaidey67@gmail.com>
2025-05-11 18:18:28 +0200

    order_packages.py

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/abdul/ros_as_ws/src'
+source_root_dir = '/home/Nimai/assignment/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/abdul/ros_as_ws/devel;/home/abdul/my_ros/devel;/opt/ros/noetic'.split(';') if '/home/abdul/ros_as_ws/devel;/home/abdul/my_ros/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/Nimai/assignment/devel;/home/abdul/my_ros/devel;/opt/ros/noetic'.split(';') if '/home/Nimai/assignment/devel;/home/abdul/my_ros/devel;/opt/ros/noetic' != '' else []

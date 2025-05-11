abd3bf7719fb78ed1d5d153425f101d95f306212
NimaiDey <Nimaidey67@gmail.com>
2025-05-11 18:09:30 +0200

    Update cmake_install.cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/abdul/ros_as_ws/src
+# Install script for directory: /home/Nimai/assignment/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/abdul/ros_as_ws/install")
+  set(CMAKE_INSTALL_PREFIX "/home/Nimai/assignment/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -49,89 +49,89 @@ endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/_setup_util.py")
+   "/home/Nimai/assignment/install/_setup_util.py")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE PROGRAM FILES "/home/abdul/ros_as_ws/build/catkin_generated/installspace/_setup_util.py")
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE PROGRAM FILES "/home/Nimai/assignment/build/catkin_generated/installspace/_setup_util.py")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/env.sh")
+   "/home/Nimai/assignment/install/env.sh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE PROGRAM FILES "/home/abdul/ros_as_ws/build/catkin_generated/installspace/env.sh")
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE PROGRAM FILES "/home/Nimai/assignment/build/catkin_generated/installspace/env.sh")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/setup.bash;/home/abdul/ros_as_ws/install/local_setup.bash")
+   "/home/Nimai/assignment/install/setup.bash;/home/Nimai/assignment/install/local_setup.bash")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE FILE FILES
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/setup.bash"
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/local_setup.bash"
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE FILE FILES
+    "/home/Nimai/assignment/build/catkin_generated/installspace/setup.bash"
+    "/home/Nimai/assignment/build/catkin_generated/installspace/local_setup.bash"
     )
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/setup.sh;/home/abdul/ros_as_ws/install/local_setup.sh")
+   "/home/Nimai/assignment/install/setup.sh;/home/Nimai/assignment/install/local_setup.sh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE FILE FILES
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/setup.sh"
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/local_setup.sh"
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE FILE FILES
+    "/home/Nimai/assignment/build/catkin_generated/installspace/setup.sh"
+    "/home/Nimai/assignment/build/catkin_generated/installspace/local_setup.sh"
     )
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/setup.zsh;/home/abdul/ros_as_ws/install/local_setup.zsh")
+   "/home/Nimai/assignment/install/setup.zsh;/home/Nimai/assignment/install/local_setup.zsh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE FILE FILES
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/setup.zsh"
-    "/home/abdul/ros_as_ws/build/catkin_generated/installspace/local_setup.zsh"
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE FILE FILES
+    "/home/Nimai/assignment/build/catkin_generated/installspace/setup.zsh"
+    "/home/Nimai/assignment/build/catkin_generated/installspace/local_setup.zsh"
     )
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/abdul/ros_as_ws/install/.rosinstall")
+   "/home/Nimai/assignment/install/.rosinstall")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/abdul/ros_as_ws/install" TYPE FILE FILES "/home/abdul/ros_as_ws/build/catkin_generated/installspace/.rosinstall")
+file(INSTALL DESTINATION "/home/Nimai/assignment/install" TYPE FILE FILES "/home/Nimai/assignment/build/catkin_generated/installspace/.rosinstall")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/abdul/ros_as_ws/build/gtest/cmake_install.cmake")
-  include("/home/abdul/ros_as_ws/build/assignment_2_2024/cmake_install.cmake")
+  include("/home/Nimai/assignment/build/gtest/cmake_install.cmake")
+  include("/home/Nimai/assignment/build/assignment_2_2024/cmake_install.cmake")
 
 endif()
 
@@ -143,5 +143,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/abdul/ros_as_ws/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/Nimai/assignment/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
